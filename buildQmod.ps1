b16fbaa2ace5adb15a9dd7e22d85a817257a5ba4
Waulta <81683867+Waulta@users.noreply.github.com>
2021-08-15 20:19:30 +0930

    Add files via upload

---
+++ buildQmod.ps1
@@ -0,0 +1,17 @@
+# Builds a .zip file for loading with BMBF
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
+Compress-Archive -Path  "./libs/arm64-v8a/libplatformmonke.so",`
+                        "./extern/libbeatsaber-hook_2_2_4.so",`
+                        "./extern/libbeatsaber-hook_2_2_5.so",`
+                        "./extern/libcustom-types.so",`
+                        "./cover.png",`
+                        "./mod.json" -DestinationPath "./platformMonke.zip" -Update 
+
+& copy-item -Force "./PlatformMonke.zip" "./PlatformMonke-2.0.2.qmod"
